2e5e8e0d825624f5166788c3eeb94802b95bb52b
zaxymelkt <cybrustech@cybrustech.net>
2024-06-22 00:19:18 -0500

    Quartz sync: Jun 22, 2024, 12:19 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "KNY : First Blood",
+    pageTitle: "KNY : Return of the Kami",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
